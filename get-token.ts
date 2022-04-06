dc28c7e17874cd2000dfa4b11cda5efaf79b30e1
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2022-04-06 16:08:23 +0200

    Fix

--- get-token.ts
+++ get-token.ts
@@ -2,23 +2,17 @@ const { readFileSync } = require("fs");
 const crypto = require("crypto");
 const ed = require("@noble/ed25519");
 const bs58 = require("bs58");
-const { BaseClient } = require("@iota/is-client");
+import { IdentityClient } from '@iota/is-client';
 import { defaultConfig } from './configuration';
 
 (async () => {
 
-    const client = new BaseClient(defaultConfig)
+    const client = new IdentityClient(defaultConfig)
 
     const identity = JSON.parse(readFileSync("./userIdentity.json").toString());
+    
+    await client.authenticate(identity.doc.id, identity.key.secret);
 
-    const body = await client.get(`authentication/prove-ownership/${identity?.doc?.id}`);
-    const nonce = body?.nonce;
-    const encodedKey = await client.getHexEncodedKey(identity?.key?.secret);
-    const signedNonce = await client.signNonce(encodedKey, nonce);
-    const { jwt } = await client.post(`authentication/prove-ownership/${identity?.doc?.id}`, {
-        signedNonce
-    });
-
-    console.log("JWT Token:", jwt);
+    console.log("JWT Token:", client.jwtToken);
 
 })()
